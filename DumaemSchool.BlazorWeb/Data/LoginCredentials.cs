abe7779ebed09703eddd1b2acb7a58a0236ca902
riazonbin <riazonbin@mail.ru>
2023-09-24 12:51:13 +0300

    SCHOOL-8 Добавить страницу авторизации с валидацией формы

--- DumaemSchool.BlazorWeb/Data/LoginCredentials.cs
+++ DumaemSchool.BlazorWeb/Data/LoginCredentials.cs
@@ -1,8 +1,13 @@
-﻿namespace DumaemSchool.BlazorWeb.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DumaemSchool.BlazorWeb.Data;
 
 public class LoginCredentials
 {
+    [Required(ErrorMessage = "Email должен быть заполнен")]
     public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Пароль должен быть заполнен")]
     public string Password { get; set; } = null!;
     public bool RememberMe { get; set; }
 }
