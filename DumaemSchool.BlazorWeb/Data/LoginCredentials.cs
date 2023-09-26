531fd6639d83674c56dfade355d9e92985b1e42b
Flictivity <bulat_sa1@mail.ru>
2023-09-26 22:58:23 +0300

    SCHOOL-8 Вынести компоненты для авторизации. Добавить таймер для отправки кода.

--- DumaemSchool.BlazorWeb/Data/LoginCredentials.cs
+++ DumaemSchool.BlazorWeb/Data/LoginCredentials.cs
@@ -5,9 +5,9 @@ namespace DumaemSchool.BlazorWeb.Data;
 public class LoginCredentials
 {
     [Required(ErrorMessage = "Email должен быть заполнен")]
-    public string Email { get; set; } = null!;
+    public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Пароль должен быть заполнен")]
-    public string Password { get; set; } = null!;
+    public string Password { get; set; } = string.Empty;
     public bool RememberMe { get; set; }
 }
