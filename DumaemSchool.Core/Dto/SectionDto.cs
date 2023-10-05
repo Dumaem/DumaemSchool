bd96ce9b3f76582e3ea11d2bd30d4e00ddaa3a20
Flictivity <bulat_sa1@mail.ru>
2023-10-05 16:45:17 +0300

    SCHOOL-13 Создана страница с кружками

---
+++ DumaemSchool.Core/Dto/SectionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DumaemSchool.Core.Dto;
+
+public sealed class SectionDto
+{
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Название группы студентов
+    /// </summary>
+    public string GroupName { get; set; } = null!;
+
+    public string SectionTypeName { get; set; }
+
+    public string TeacherName { get; set; } = null!;
+}
