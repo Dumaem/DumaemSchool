9c60bcda6287820903cff40d45795b829c7b1450
riazonbin <riazonbin@mail.ru>
2023-10-07 09:58:05 +0300

    SCHOOL-14 Правки по PR

--- DumaemSchool.Database/Repositories/ITeacherRepository.cs
+++ DumaemSchool.Database/Repositories/ITeacherRepository.cs
@@ -5,7 +5,7 @@ namespace DumaemSchool.Database.Repositories;
 
 public interface ITeacherRepository
 {
-    public Task<ListDataResult<TeacherDto>> ListTeachersAsync(bool includeFired, ListParam listParam);
+    public Task<ListDataResult<TeacherDto>> ListTeachersAsync(ListParam listParam);
     public Task<Core.Models.Teacher> AddTeacherAsync(Core.Models.Teacher teacher);
     public Task<bool> DeleteTeacherAsync(int id);
 }
