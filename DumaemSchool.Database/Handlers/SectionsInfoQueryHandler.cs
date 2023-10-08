8460659e8edb4eb3e09d7cb882f6d63b622f86f2
Flictivity <bulat_sa1@mail.ru>
2023-10-08 18:01:10 +0300

    SCHOOL-13 Реализована страница кружков (отображение) Добавлен компонент для отображения кружков

--- DumaemSchool.Database/Handlers/SectionsInfoQueryHandler.cs
+++ DumaemSchool.Database/Handlers/SectionsInfoQueryHandler.cs
@@ -5,16 +5,16 @@ using MediatR;
 
 namespace DumaemSchool.Database.Handlers;
 
-public sealed class TeacherSectionsInfoQueryHandler : IRequestHandler<TeacherSectionsInfoQuery, ListDataResult<SectionInfo>>
+public sealed class SectionsInfoQueryHandler : IRequestHandler<SectionsInfoQuery, ListDataResult<SectionInfo>>
 {
     private readonly ISectionRepository _repository;
 
-    public TeacherSectionsInfoQueryHandler(ISectionRepository repository)
+    public SectionsInfoQueryHandler(ISectionRepository repository)
     {
         _repository = repository;
     }
 
-    public async Task<ListDataResult<SectionInfo>> Handle(TeacherSectionsInfoQuery request, 
+    public async Task<ListDataResult<SectionInfo>> Handle(SectionsInfoQuery request, 
         CancellationToken cancellationToken)
     {
         return await _repository.ListSectionInfo(request.Param);
