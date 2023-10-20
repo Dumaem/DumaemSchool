1467155e9596fa757ce8f51a85b117a3eabebfc3
Flictivity <bulat_sa1@mail.ru>
2023-10-20 19:25:47 +0300

    SCHOOL-28 Исправлены ошибки устаноавления пароля

---
+++ DumaemSchool.BlazorWeb/Data/PasswordSettingCredentials.cs
@@ -0,0 +1,12 @@
+﻿namespace DumaemSchool.BlazorWeb.Data;
+
+public class PasswordSettingCredentials
+{
+    [Required]
+    [MinLength(5)]
+    public string NewPassword { get; set; } = null!;
+    
+    [Required]
+    [MinLength(5)]
+    public string NewPasswordRepeat { get; set; } = null!;
+}
